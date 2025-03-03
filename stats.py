7b86a608d4b8b858970beda31077a4178a4c2d37
HunTopher <HunTopher1588@gmail.com>
2025-03-03 06:57:43 -0800

    fixed bookbot... again

--- stats.py
+++ stats.py
@@ -1,71 +1,3 @@
-.gitignore
-U
-
-README.md
-U
-
-main.py
-M
-
-stats.py
-M
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -92,4 +24,3 @@ def chars_dict_to_sorted_list(num_chars_dict):
         sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
-
